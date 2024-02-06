df4ddef977187b370d517419a13e575610b435f2
Sahaj Pal Singh Mahla <54036040+Sahajpal@users.noreply.github.com>
2024-02-07 03:41:37 +0530

    style: Made use of Figtree font as given in the figma

--- app/src/main/java/com/sahaj/openinappassignment/ui/theme/Type.kt
+++ app/src/main/java/com/sahaj/openinappassignment/ui/theme/Type.kt
@@ -2,33 +2,23 @@ package com.sahaj.openinappassignment.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sahaj.openinappassignment.R
 
-// Set of Material typography styles to start with
+val figtree = FontFamily(
+    listOf(
+        Font(R.font.figtree_regular, FontWeight.Normal),
+        Font(R.font.figtree_medium, FontWeight.Medium),
+        Font(R.font.figtree_semibold, FontWeight.SemiBold),
+        Font(R.font.figtree_bold, FontWeight.Bold),
+        Font(R.font.figtree_black, FontWeight.Black),
+        Font(R.font.figtree_light, FontWeight.Light)
+    )
+)
+// Would have used the typography but would have taken a lot of time to study and tally the sizes for each classification
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    //TODO
 )
